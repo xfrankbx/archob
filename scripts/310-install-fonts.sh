94f5bb86a62103de2e4dacdcccc16982fa115073
Frank B <fboyd78@gmail.com>
2020-06-30 16:14:39 +0000

    Added fonts and others

---
+++ scripts/310-install-fonts.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+echo "################################################################"
+echo "###                      Installing fonts                    ###"
+echo "################################################################"
+
+
+fonts=(
+# pacman
+awesome-terminal-fonts
+adobe-source-sans-pro-fonts
+terminus-font
+ttf-ubuntu-font-family
+# trizen
+font-manager-git
+ttf-ms-fonts
+urxvt-resize-font-git
+)
+
+for font in ${fonts[@]}
+do
+  if pacman -Qi $package &> /dev/null; then
+    echo "################################################################"
+    echo "###              $package is already installed               ###"
+    echo "################################################################"
+  else
+    echo "################################################################"
+    echo "###                   Installing $package                    ###"
+    echo "################################################################"
+    trizen -S --noconfirm --needed $font
+done
