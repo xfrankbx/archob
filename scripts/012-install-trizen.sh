92c88eb3f4fab2f998bb50d37aa3cb09995cffc2
Frank B <fboyd78@gmail.com>
2020-06-30 21:50:39 +0000

    Fix mistake

--- scripts/012-install-trizen.sh
+++ scripts/012-install-trizen.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo pacman -S --noconfirm --neded git fakeroot binutils
+sudo pacman -S --noconfirm --needed git fakeroot binutils
 git clone https://aur.archlinux.org/trizen-git.git /tmp/trizen-git
 cd /tmp/trizen-git
 makepkg --noconfirm --needed -si
